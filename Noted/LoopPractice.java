3267b2b67e1aa8df260d669a1e5aba5bd67afc67
rmrjSP <ramrage13@gmail.com>
2022-09-01 11:05:06 -0700

    9/1 finished nested loops

--- Noted/LoopPractice.java
+++ Noted/LoopPractice.java
@@ -16,14 +16,25 @@ public class LoopPractice
         }
     }
     public static void practice1(){
-        for(int i = 1; i <= 5; i++){
-          System.out.println("1 2 3 4 5");
+        for(int i = 0; i <= 5; i++){
+          for(int j = 1; j <= 5; j++){
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
     public static void practice3(){
           for(int i = 1; i <= 5; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void practice4(){
+          for(int i = 5; i >= 1; i--){
+            for(int j = 5; j >= i; j--){
+                System.out.print(i + " ");
             }
             System.out.println();
         }
