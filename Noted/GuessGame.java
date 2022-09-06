24566e58e2af25c5ae30ee40ddb6ee323f3983cd
rmrjSP <ramrage13@gmail.com>
2022-09-06 11:17:54 -0700

    9/6/22

--- Noted/GuessGame.java
+++ Noted/GuessGame.java
@@ -1,4 +1,5 @@
-
+import java.lang.Math;
+import java.util.Scanner;
 /**
  * Write a description of class GuessGame here.
  *
@@ -7,7 +8,22 @@
  */
 public class GuessGame
 {
-    public static void guessGame(){
-        
+    public static String guessGame(){
+        int num = (int)((Math.random()  * 10) + 1);
+          Scanner scanner = new Scanner(System.in);
+        System.out.println("what is your guess");
+        for (int i = 0; i < 5; i++){
+           
+        int guess = scanner.nextInt();
+        if (guess == num){
+            System.out.println( "Game won");
+            return "Game Won";
+           }else if (guess > num){
+               System.out.println("Guess is to high");
+            }else if (guess < num){
+               System.out.println("Guess is to Low");
+            }
+        }
+        return "Game Lost";
     }
 }
