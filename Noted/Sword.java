11a846b10f35f0df72f099ad5145ecd811bb3930
rmrjSP <ramrage13@gmail.com>
2022-09-12 11:13:13 -0700

    objects

--- Noted/Sword.java
+++ Noted/Sword.java
@@ -13,6 +13,8 @@ public class Sword
     private double size;
     private Color scabbardColor;
     private int durability = 100;
+    private int sharpness = 100;
+    private int kills = 0;
 
 
     /**
@@ -48,5 +50,37 @@ public class Sword
      public double getSize(){
         return size;
     }
+    public Color getScabbardColor(){
+        return scabbardColor;
+    }
     
+    
+    public void setMetal(String newMetal){
+        metal = newMetal;
+    }
+    public void setType(String newType){
+        type = newType;
+    }
+    public void setOwner(String newOwner){
+        owner = newOwner;
+    }
+    public void setSize(double newSize){
+        size = newSize;
+    }
+    public void setColor(Color newScabbardColor){
+        scabbardColor = newScabbardColor;
+    }
+    
+    public void breakSword(){
+        durability = 0;
+        System.out.println("It broke");
+    }
+    public void dullSword(){
+        sharpness = 0;
+        System.out.println("It's dull");
+    }
+    public void gotKill(){
+        kills--;
+        System.out.println("You got a kill");
+    }
 }
