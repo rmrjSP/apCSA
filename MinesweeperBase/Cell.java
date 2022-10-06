beb64fbf59e0dd22099d9a038a95b42509b845de
rmrjSP <ramrage13@gmail.com>
2022-10-06 11:38:09 -0700

    10/6

--- MinesweeperBase/Cell.java
+++ MinesweeperBase/Cell.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionListener;
 
 public class Cell implements ActionListener{
     //Variables you need to work with
-    int value;
+    private int value;
     
     //Variables you don't need to worry about or care about.
     private JButton button;
@@ -39,6 +39,10 @@ public class Cell implements ActionListener{
     public int setMine(){
         return value = 10;
     }
+    public int getValue(){
+      return value;
+    }
+    
     //The following methods are used for the User Inferface. These methods are fully functional and do not need to be modified.
     public void checkCell(){
         button.setEnabled(false);
--- MinesweeperBase/MinesweeperBoard.java
+++ MinesweeperBase/MinesweeperBoard.java
@@ -14,54 +14,74 @@ public class MinesweeperBoard{
     Cell[] board;
     int rows;
     int columns;
-    
+
     public MinesweeperBoard(int row, int col ){
         //Put the constructor here.
         board = new Cell[row * col];
         rows = row;
         columns = col;
-        
+
         //These pieces are for the GUI.
         JFrame frame = new JFrame();
         frame.add(addCells());
-        addMines(20);
+        addMines(1);
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
     public MinesweeperBoard(){
-	this(10,10);
+        this(10,10);
     }
 
     public void addMines(int mines) {
-        
-        
-        for(int i = 0; i < mines; i++){
-          int mineCell = (int)(Math.random() * (rows*columns) +1);
-          board[mineCell].setMine();
-        }
-        
 
+        for(int i = 0; i < mines; i++){
+            int mineCell = (int)(Math.random() * (rows*columns) +1);
+            board[mineCell].setMine();
+        }
+
+    }
+
+    public int getIndex(){
+        int x = 0;
+        for( int i = 0; i < (rows*columns) ; i++){
+            if(board[i] == 
+        }
+        return x;
     }
 
     public void addNums(){
         
     }
+
     /**This method is used for testing and will be deleted if using the GUI.
      *  It is still required for all students.
      */
     public void printBoard(){
-        for(int i = 0; i < rows; i++){
-           System.out.print(board[i].value );
+        int i;
+        int p;
+        int c = -1;
+        for( p = 0; p < columns; p++){
+            for( i = 0; i < rows; i++){
+                c++;
+                if(board[c].getValue() == 10){
+                    System.out.print("X");
+                }else{
+                    System.out.print(board[c].getValue() );
+                }
+            }
+            System.out.println("");
         }
-}
+    }
+
     public JPanel addCells(){
         JPanel panel = new JPanel(new GridLayout(rows,columns));
         for(int i = 0; i< rows*columns; i++){
-                board[i]= new Cell();
-                panel.add(board[i].getButton());
+            board[i]= new Cell();
+            panel.add(board[i].getButton());
+
         }
         return panel;
     }
-
 }
